e2fe10b9b3d8315be2e3f655a7a2e0b4e3b0ae51
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-06 14:35:39 +0100

    CORS

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+devServer: {
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+}
+
 module.exports = environment.toWebpackConfig()
