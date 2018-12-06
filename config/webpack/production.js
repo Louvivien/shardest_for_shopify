573a57911ca811ae9b1a7f94d0a97fa1e6e2ce37
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-06 14:48:14 +0100

    cors gem

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,10 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-devServer: {
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
-}
-
 module.exports = environment.toWebpackConfig()
