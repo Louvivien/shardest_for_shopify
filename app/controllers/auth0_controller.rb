a0e483c4b2cd027cf87c5a127be688912197890b
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-07 18:49:05 +0100

    auth + proxy 7

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -6,7 +6,7 @@ class Auth0Controller < ApplicationController
     session[:userinfo] = request.env['omniauth.auth']
 
     # Redirect to the URL you want after successful auth
-    redirect_to controller: 'accounts', action: 'home', session[:userinfo] : session[:userinfo]
+    redirect_to controller: 'accounts', action: 'home', session: session
   end
 
   def failure
