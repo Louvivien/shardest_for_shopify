7e92d7e001e75ecc6c96e5d98706c2dcfe6a4d54
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-07 18:27:03 +0100

    auth + proxy 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'auth/oauth2/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
   #root :to => 'app_proxy#index'
-  get '/' => 'accounts#home'
+  get '/' => 'auth0#callback'
+  get '/account' => 'accounts#home'
  
       
     # GET /app_proxy/reviews will now be routed to
