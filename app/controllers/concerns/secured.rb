b7520d966491ece30ddb3064fefffa8eb6b9ad4a
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-08 14:25:29 +0100

    auth14

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -6,6 +6,6 @@ module Secured
   end
 
   def logged_in_using_omniauth?
-    redirect_to 'https://shardest.com/a/account/auth/auth0', shop: 'shop', path_prefix: 'path_prefix', timestamp: 'timestamp',  signature: 'signature' unless session[:userinfo].present?
+    redirect_to 'https://shardest.com/a/account/auth/auth0/callback', shop: 'shop', path_prefix: 'path_prefix', timestamp: 'timestamp',  signature: 'signature' unless session[:userinfo].present?
   end
 end
