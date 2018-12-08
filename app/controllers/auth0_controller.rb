1a19c69042b40918588655e0b09fcd31fd0632b5
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-08 14:09:36 +0100

    auth13

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -6,7 +6,7 @@ class Auth0Controller < ApplicationController
     session[:userinfo] = request.env['omniauth.auth']
 
     # Redirect to the URL you want after successful auth
-    redirect_to 'https://shardest.com/a/account/callback',  shop: 'shop', path_prefix: 'path_prefix', timestamp: 'timestamp',  signature: 'signature'
+    redirect_to 'https://shardest.com/a/account',  shop: 'shop', path_prefix: 'path_prefix', timestamp: 'timestamp',  signature: 'signature'
   end
 
   def failure
