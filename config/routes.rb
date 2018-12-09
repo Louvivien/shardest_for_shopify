118acd94749225e1513d762bfe70292e8e737819
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-09 10:34:33 +0100

    inter log12

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get 'accounts/home'
 
   mount ShopifyApp::Engine, at: '/'
-  get '/auth/oauth2/*callback*', to: 'auth0#callback'
+  get '/auth/oauth2/*', to: 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   #root :to => 'app_proxy#index'
   root :to => 'accounts#home'
