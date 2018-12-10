c494a81697702939c180aa8a08a08734272907a8
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-10 17:40:18 +0100

    lock redirect11

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/auth/failure' => 'auth0#failure'
   #root :to => 'app_proxy#index'
   get '/home' => 'accounts#home'
-  get '/' => 'app_proxy#index'
+  root :to => 'app_proxy#index'
   
  
       
