48e5bd4d9ffbf23dd255c1f8bf7c286eac807259
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-10 18:51:16 +0100

    lock redirect22

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,9 @@
 class AccountsController < ApplicationController
   include ShopifyApp::AppProxyVerification
-  before_action :authenticate_user!
+
 
   def home
-    include Auth0Helper
+    
   end
 
 end
