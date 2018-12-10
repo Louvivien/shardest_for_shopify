829b4a7908bb8c478009a437f3cf61107a9c207b
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-10 21:01:46 +0100

    auth grant flow

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,11 +1,29 @@
 class Auth0Controller < ApplicationController
   include ShopifyApp::AppProxyVerification
-	def callback
+  def callback
+    
+    require 'uri'
+    require 'net/http'
+
+      url = URI("https://shardest.eu.auth0.com/oauth/token")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Post.new(url)
+      request["content-type"] = 'application/json'
+      request.body = "{\"grant_type\":\"authorization_code\",\"client_id\": \"ap1RB7pH8lSi-hdvq3W_tD-7IwIK7yov\",\"client_secret\": \"#{Rails.application.credentials.auth0_client_secret}\",\"code\": \"#{params[:code]}\",\"redirect_uri\": \"https://shardest.com/a/account/auth/oauth2/callback\"}"
+
+      response = http.request(request)
+      puts response.read_body
+
+
     # This stores all the user information that came from Auth0
     # and the IdP
-    session[:userinfo] = request.env['omniauth.auth']
-    # Redirect to the URL you want after successful auth
-    redirect_to '/a/account/dashboard',  shop: 'shop', path_prefix: 'path_prefix', timestamp: 'timestamp',  signature: 'signature'
+    # session[:userinfo] = request.env['omniauth.auth']
+    # # Redirect to the URL you want after successful auth
+    # redirect_to '/a/account/dashboard',  shop: 'shop', path_prefix: 'path_prefix', timestamp: 'timestamp',  signature: 'signature'
   end
 
   def failure
--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,12 +1,12 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider(
-      :auth0,
-      Rails.application.credentials.auth0_client_id,
-      Rails.application.credentials.auth0_client_secret,
-      'shardest.eu.auth0.com',
-      #callback_url: 'https://shardest.com/a/account/auth/oauth2/callback',
-      authorize_params: {
-        scope: 'openid profile'
-      }
-    )
-  end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#     provider(
+#       :auth0,
+#       Rails.application.credentials.auth0_client_id,
+#       Rails.application.credentials.auth0_client_secret,
+#       'shardest.eu.auth0.com',
+#       callback_url: 'https://shardest.com/a/account/auth/oauth2/callback',
+#       authorize_params: {
+#         scope: 'openid profile'
+#       }
+#     )
+#   end
