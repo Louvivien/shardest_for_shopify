0eb1a121016d23a00d67c50e94bd3250d859b1db
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-10 18:17:27 +0100

    lock redirect17

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,10 @@
 class AccountsController < ApplicationController
   include ShopifyApp::AppProxyVerification
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   def home
+    puts session[:userinfo]
+    @current_user = session[:userinfo]
     @user = current_user
   end
 
