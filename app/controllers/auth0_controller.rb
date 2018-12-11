5cf009142d21a76533df157ed8f5597f4b3e28c1
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-11 03:12:11 +0100

    session impl675

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -46,7 +46,7 @@ class Auth0Controller < ApplicationController
     puts session[:userinfo] = decoded_token[0]
     puts session[:userinfo]
     @user = session[:userinfo]
-    puts session[:userinfo].class
+    puts type(session[:userinfo])
     # Redirect to the URL you want after successful auth
     #redirect_to '/a/account/dashboard',  shop: 'shop', path_prefix: 'path_prefix', timestamp: 'timestamp',  signature: 'signature'
 
