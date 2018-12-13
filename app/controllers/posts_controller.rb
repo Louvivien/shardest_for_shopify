f39c4d53cc2ef2212b83a60ca498bc461bc95e4a
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-13 21:20:28 +0100

    post 2 params5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -99,6 +99,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:image_url, :product1_url)
+      params.require(:post).permit(:image_url, :product1_url, :image)
     end
 end
