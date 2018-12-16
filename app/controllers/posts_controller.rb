0a58b4499765643a9c69bb8980d9fb5285e33f44
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-16 19:56:41 +0100

    ajax delete 12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -72,6 +72,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    puts params
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
