ad4f51390ee586a67213be6b6f136559b6b733db
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-16 23:36:22 +0100

    migration published status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_091109) do
+ActiveRecord::Schema.define(version: 2018_12_16_222913) do
 
   create_table "posts", force: :cascade do |t|
     t.string "image_url"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_091109) do
     t.string "product5_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published"
   end
 
   create_table "shops", force: :cascade do |t|
