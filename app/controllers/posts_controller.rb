8790d7c76252013bb262a6e9fc0e830ead8e0cb1
Nicolas Gervais <gervais.nicolas@live.fr>
2018-12-17 00:34:00 +0100

    share ajax9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
 
   def publish
     @post = Post.find(params[:post_id])
-    @post.update_attribute = (post_params)
+    @post.update_attribute(post_params)
     # respond_to do |format|
     #   format.html
     #   format.js {}
